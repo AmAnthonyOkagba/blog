8c018bd917a2ec08bd8facd6ad864a2b989cf7f0
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2021-08-27 06:26:41 +0100

    created the navbar

---
+++ resources/views/layouts/inc/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow ">
+    <div class="container">
+        <a class="navbar-brand" href="/">CapableBohz</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('about') }} ">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('employee') }}">Employee</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('contact') }}">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
