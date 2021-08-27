db7baf3a6bc4e104bcb95a94757baca9e86a7321
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2021-08-27 17:16:59 +0100

    updated the home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.base')
 
 @section('content')
-    <h1>Hello World</h1>
+    <div class="container pt-5 text-center">
+        <div class="row">
+            <div class="card">
+                <div class="card-body">
+                    <h1>HELLO WORLD</h1>
+                    <p>This is the home page</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    
 @endsection
